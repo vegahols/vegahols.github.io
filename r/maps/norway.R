034f2240c6190558650b44bb2821c914e3afad45
Vegahols <vegard.holte.olsen@hkdir.no>
2025-11-27 22:17:48 +0100

    Fix Norway map - correct Highcharts Maps module path

--- r/maps/norway.R
+++ r/maps/norway.R
@@ -39,7 +39,7 @@ generate_norway_chart <- function() {
   <meta charset="utf-8"/>
   <title>Norway Regional Population</title>
   <script src="https://code.highcharts.com/highcharts.js"></script>
-  <script src="https://code.highcharts.com/modules/map.js"></script>
+  <script src="https://code.highcharts.com/maps/modules/map.js"></script>
   <script src="https://code.highcharts.com/modules/exporting.js"></script>
   <style>
     body {
